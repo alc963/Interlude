b5406aa670e756a6ac2d63ddefe9eff5a42f0c68
Chairmichael <jeffvhenry@gmail.com>
2026-09-11 09:10:46 -0700

    Initial Spring Boot project boilerplate from spring initializr

---
+++ backend/interludeapp/src/main/java/com/naugroup3/interludeapp/InterludeappApplication.java
@@ -0,0 +1,13 @@
+package com.naugroup3.interludeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterludeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterludeappApplication.class, args);
+	}
+
+}
---
+++ backend/interludeapp/src/test/java/com/naugroup3/interludeapp/InterludeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naugroup3.interludeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterludeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
