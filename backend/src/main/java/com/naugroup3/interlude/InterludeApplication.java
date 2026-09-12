df3916fde228be759a3bffb0d9053acc9fd3861d
Illusion137 <raygoza.d@hotmail.com>
2026-09-11 17:59:22 -0700

    feat(Backend): add a simple ping on the empty route `('/')`

--- backend/src/main/java/com/naugroup3/interlude/InterludeApplication.java
+++ backend/src/main/java/com/naugroup3/interlude/InterludeApplication.java
@@ -2,18 +2,23 @@ package com.naugroup3.interlude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
+@RestController
 public class InterludeApplication implements CommandLineRunner {
+	@GetMapping("/")
+	public String ping() {
+		return "Interlude server is online";
+	}
 
 	public void run(String args[]) throws Exception {
-		System.out.println("Hello world");
+		System.out.println("Ready...");
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterludeApplication.class, args);
 	}
-
 }
