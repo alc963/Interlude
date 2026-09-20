1f8e63434dcd3d382578aaf41a70e27635d35ff4
Jeff Henry <jeffvhenry@gmail.com>
2026-09-20 14:22:53 -0700

    frontend to backend to db poc. proxy server kinks to settle out

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: 'localhost',
+    port: 8081,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8082/api',
+        changeOrigin: true,
+      },
+    },
+  },
 })
